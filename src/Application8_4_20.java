b2b4ea0985a2b877d52ae4d7a52b5910a255fbf3
Munni <Munni@DESKTOP-77233KH>
2021-01-20 14:13:40 -0500

    test 1

---
+++ src/Application8_4_20.java
@@ -0,0 +1,28 @@
+
+public class Application8_4_20 {
+
+	public static void main(String[] args) {
+		//System.out.println("Hello World!");
+		//declaring variables and primitive language for Java: built in language : 8 Primitive Language 
+		int myNumber = 88;
+		short myShort = 100;
+		long myLong = 500;
+		double myDouble = 800;
+		float myFloat =900f;
+		char myChar = 'y';
+		boolean myBoolean = false;
+		byte myByte = 127;
+				
+		System.out.println(myNumber);
+		System.out.println(myShort);
+		System.out.println(myLong);
+		System.out.println(myDouble);
+		System.out.println(myFloat);
+		System.out.println(myChar);
+		System.out.println(myBoolean);
+		System.out.println(myByte);
+		
+		
+	}
+
+}
---
+++ src/Tutorial_8_5_20.java
@@ -0,0 +1,13 @@
+
+public class Tutorial_8_5_20 {
+	public static void main(String[] args) {
+		int myInt = 7;
+		String text = "Hello"; ///WORKING WITH STRINGS AND TEXT 
+		String blank = " ";
+		String name = "Bob";
+		String greeting = text + blank + name;
+		
+		System.out.println(greeting);
+		
+	}
+}
