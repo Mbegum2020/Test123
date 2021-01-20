6e4a412178d29279944c0a5c738f07e2d04bcba9
Munni <Munni@DESKTOP-77233KH>
2021-01-20 14:07:52 -0500

    new

---
+++ src/WhileLoop.java
@@ -0,0 +1,20 @@
+
+public class WhileLoop {////WHILE LOOPS 
+	public static void main(String[] args) {
+		//boolean loop = true;
+		//boolean loop = 4 < 5;
+		//System.out.println(loop);
+		
+		//int value = 10;
+		//boolean loop = value < 20;
+		//System.out.println(loop);
+		
+		int value = 0;
+			while(value < 10)
+				System.out.println("Hello" +  value);
+			
+				value = value + 1;
+		
+	}
+}
+
